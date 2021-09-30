ae40e13a3eb1522f95a978e60113d5ba64b2144a
siva kondalu rajulapati <sivacool145@gmail.com>
2021-09-30 10:41:39 +0530

    WH - 8860 - ANOTHER UPDATE

--- src/main/java/in/nit/sivakondalu/MessageRunner.java
+++ src/main/java/in/nit/sivakondalu/MessageRunner.java
@@ -3,6 +3,8 @@ package in.nit.sivakondalu;
 
 
 public class MessageRunner {
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -11,7 +13,7 @@ public class MessageRunner {
 		System.out.println("WELCOME TO SIVA KONDALU WITH GOOD PACKAGE !!!");
 		System.out.println("CONGRATULATIONS !!!");
 		System.out.println("WE WRE EXCITED TO WORK WITH YOU !!!");
-		System.out.println("AGAIN WELCOME TO SIVA KONDALU TO OUR NEW PROJECT !!!");
+		
 	}
 
 }
